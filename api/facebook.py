01c7a9d4b6c32fe773e417c60482cea737fc65a0
Sotheara@SOTHEARA-PC <sieng.sotheara.7@gmail.com>
2017-09-10 22:40:02 +0700

    add authorized to retain user info email

--- api/facebook.py
+++ api/facebook.py
@@ -21,7 +21,7 @@ facebook = oauth.remote_app('facebook',
     authorize_url='https://www.facebook.com/dialog/oauth',
     consumer_key=FACEBOOK_APP_ID,
     consumer_secret=FACEBOOK_APP_SECRET,
-    request_token_params={'scope': 'email,public_profile,user_friends'}
+    request_token_params={'scope': 'email'}
 )
 
 
@@ -47,9 +47,9 @@ def facebook_authorized(resp):
         )
     #print (resp['access_token'], '')
     session['oauth_token'] = (resp['access_token'], '')
-    me = facebook.get('/me')
-    return 'Logged in as id=%s name=%s redirect=%s' % \
-        (me.data['id'], me.data['name'], request.args.get('next'))
+    me = facebook.get('/me?fields=id,name,email')
+    return 'Logged in as id=%s name=%s email=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], me.data['email'], request.args.get('next'))
 
 
 @facebook.tokengetter
